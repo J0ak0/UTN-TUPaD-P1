bc240b64fd64683216870a7af06764937aa4a923
Joako <joavankergmail.com>
2025-05-10 14:06:56 -0300

    [UPD] Hasta ejercio 9

--- 04 Estructuras Repetitivas/VANKEIRSBILCK_TP-04.py
+++ 04 Estructuras Repetitivas/VANKEIRSBILCK_TP-04.py
@@ -52,4 +52,70 @@ while numero_usuario != numero_aleatorio:
 
 print("Has adivinado, el número era", numero_aleatorio," y necesitaste", intentos, "intentos.")
 
+# Ejercicio 6: Desarrolla un programa que imprima en pantalla todos los números pares comprendidos
+# entre 0 y 100, en orden decreciente.
+
+for i in range(100, -1, -1):
+    if i % 2 == 0:
+        print(i)
+
+
+# Ejercicio 7: Crea un programa que calcule la suma de todos los números comprendidos entre 0 y un
+# número entero positivo indicado por el usuario.
+
+numero = int(input("Ingrese un número entero positivo: "))
+suma = 0
+for i in range(numero + 1):
+    suma = suma + i
+print(f"La suma de los números entre 0 y {numero} es: {suma}")
+
+# Ejercio 8: ) Escribe un programa que permita al usuario ingresar 100 números enteros. Luego, el
+# programa debe indicar cuántos de estos números son pares, cuántos son impares, cuántos son
+# negativos y cuántos son positivos. (Nota: para probar el programa puedes usar una cantidad
+# menor, pero debe estar preparado para procesar 100 números con un solo cambio).
+
+
+CANTIDAD = 10 # Definir la cantidad de números a ingresar 
+pares = 0
+impares = 0
+positivos = 0
+negativos = 0
+
+for i in range(CANTIDAD):
+    numero = int(input(f"Ingresá el número {i + 1}: "))
+
+    # pares e impares
+    if numero % 2 == 0:
+        pares = pares + 1
+    else:
+        impares = impares + 1
+
+    # positivos y negativos
+    if numero > 0:
+        positivos = positivos + 1
+    elif numero < 0:
+        negativos = negativos + 1
+
+print("\n")
+print("Resultados:")
+print("Cantidad de números pares:", pares)
+print("Cantidad de números impares:", impares)
+print("Cantidad de números positivos:", positivos)
+print("Cantidad de números negativos:", negativos)
+
+# Ejercicio 9: Elabora un programa que permita al usuario ingresar 100 números enteros y luego calcule la
+# media de esos valores. (Nota: puedes probar el programa con una cantidad menor, pero debe
+# poder procesar 100 números cambiando solo un valor).
+
+CANTIDAD = 10  # Definir la cantidad de números a ingresar 
+
+suma = 0
+
+for i in range(CANTIDAD):
+    numero = int(input(f"Ingresá el número {i + 1}: "))
+    suma = suma + numero
+
+media = suma / CANTIDAD
+
+print("\nLa media de los", CANTIDAD, "números ingresados es:", media)
 
