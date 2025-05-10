d533615ef694c993ff05047bf9d350ebb7fba746
Joako <joavankergmail.com>
2025-05-10 16:24:13 -0300

    [UPD] Hice todo

--- 05 Funciones/VANKEIRSBILCK_TP-05.py
+++ 05 Funciones/VANKEIRSBILCK_TP-05.py
@@ -82,3 +82,22 @@ print("La lista de dobles es:", dobles)
 # d) Imprimir la lista resultante por pantalla
 
 compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],["agua"]]
+compras[2].append("jugo")
+compras[1][1] = "tallarines"
+compras[0].remove("pan")
+print("La lista de compras es:", compras)
+
+# Ejercicio 10: Elaborar una lista anidada llamada “lista_anidada” que contenga los siguientes elementos:
+# Posición lista_anidada[0]: 15
+#  Posición lista_anidada[1]: True
+#  Posición lista_anidada[2][0]: 25.5
+#  Posición lista_anidada[2][1]: 57.9
+#  Posición lista_anidada[2][2]: 30.6
+#  Posición lista_anidada[3]: False
+# Imprimir la lista resultante por pantalla.
+
+lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
+print("La lista anidada es:", lista_anidada)
+
+
+
