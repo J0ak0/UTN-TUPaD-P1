3113b6a734b46a747d526e2ec654e27810f65f90
Joako <joavankergmail.com>
2025-05-10 16:20:39 -0300

    [UPD] hasta la 8 y comence la 9

--- 05 Funciones/VANKEIRSBILCK_TP-05.py
+++ 05 Funciones/VANKEIRSBILCK_TP-05.py
@@ -47,7 +47,9 @@ Luego, identifica el número más grande, para ello se utiliza "max(numeros)" y 
 El resultado final que se muestra es la lista sin su elemento más grande: numeros = [8,15,3,7].
 """)
 
-# Ejercicio 6: 
+# Ejercicio 6: Crear una lista con números del 10 al 30 (incluído), haciendo saltos de 5 en 5 y mostrar por
+# pantalla los dos primeros.
+
 
 numeros = list(range(10,31,5))
 print("La lista de números es:", numeros)
